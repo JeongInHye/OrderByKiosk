2f57c1cb40c69b47daf6d241cdeee5101f0a9dca
정인혜 <inhyea36@naver.com>
2018-12-13 17:33:24 +0900

    button,panel 사이즈 맞춤

--- OBK/OBK/Views/AdminView/MenuSettingView.cs
+++ OBK/OBK/Views/AdminView/MenuSettingView.cs
@@ -1,7 +1,9 @@
-﻿using OBK.Modules;
+﻿using OBK.Forms.AdminForm;
+using OBK.Modules;
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,12 +13,11 @@ namespace OBK.Views.AdminView
 {
     class MenuSettingView
     {
-        private Draw draw;
-        private Form parentForm;
-        private Label label1, label2, label3, label4, label5, label6;
-        private ListView list;
-        private Button button;
         private Hashtable hashtable;
+        private Draw draw;
+        private Form parentForm, tagetForm;
+        private Button btnAdd, btnEdit, btnDelete,btnMain;
+        private Panel contents;
 
         public MenuSettingView(Form parentForm)
         {
@@ -27,6 +28,76 @@ namespace OBK.Views.AdminView
         }
 
         private void getView()
+        {
+            hashtable = new Hashtable();
+            hashtable.Add("size", new Size(140, 100));
+            hashtable.Add("point", new Point(20, 20));
+            hashtable.Add("color", Color.LightGray);
+            hashtable.Add("name", "btnAdd");
+            hashtable.Add("text", "메뉴추가");
+            hashtable.Add("font", new Font("맑은 고딕", 14, FontStyle.Regular));
+            hashtable.Add("click", (EventHandler)btnAdd_Click);
+            btnAdd = draw.getButton1(hashtable, parentForm);
+            btnAdd.BackColor = Color.FromArgb(46, 204, 113);
+
+            hashtable = new Hashtable();
+            hashtable.Add("size", new Size(140, 100));
+            hashtable.Add("point", new Point(20, 140));
+            hashtable.Add("color", Color.LightGray);
+            hashtable.Add("name", "btnEdit");
+            hashtable.Add("text", "메뉴수정");
+            hashtable.Add("font", new Font("맑은 고딕", 14, FontStyle.Regular));
+            hashtable.Add("click", (EventHandler)btnAdd_Click);
+            btnEdit = draw.getButton1(hashtable, parentForm);
+
+            hashtable = new Hashtable();
+            hashtable.Add("size", new Size(140, 100));
+            hashtable.Add("point", new Point(20, 260));
+            hashtable.Add("color", Color.LightGray);
+            hashtable.Add("name", "btnDelete");
+            hashtable.Add("text", "메뉴삭제");
+            hashtable.Add("font", new Font("맑은 고딕", 14, FontStyle.Regular));
+            hashtable.Add("click", (EventHandler)btnAdd_Click);
+            btnDelete = draw.getButton1(hashtable, parentForm);
+
+
+            hashtable = new Hashtable();
+            hashtable.Add("size", new Size(140, 60));
+            hashtable.Add("point", new Point(20, 480));
+            hashtable.Add("color", Color.LightGray);
+            hashtable.Add("name", "btnMain");
+            hashtable.Add("text", "메인화면");
+            hashtable.Add("font", new Font("맑은 고딕", 14, FontStyle.Regular));
+            hashtable.Add("click", (EventHandler)btnMain_Click);
+            btnMain = draw.getButton1(hashtable, parentForm);
+
+            hashtable = new Hashtable();
+            hashtable.Add("type", "");
+            hashtable.Add("size", new Size(680, 520));
+            hashtable.Add("point", new Point(180, 20));
+            hashtable.Add("color", Color.White);
+            hashtable.Add("name", "head");
+            contents = draw.getPanel(hashtable, parentForm);
+            contents.BorderStyle = BorderStyle.FixedSingle;
+
+
+        }
+
+        
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            btnAdd.BackColor = Color.FromArgb(46, 204, 113);
+            btnAdd.BackColor = Color.LightGray;
+            btnAdd.BackColor = Color.LightGray;
+
+            if (tagetForm != null) tagetForm.Dispose();
+
+            tagetForm = draw.getMdiForm(parentForm, new MenuAddForm(), contents);
+            tagetForm.Show();
+        }
+
+        private void btnMain_Click(object sender, EventArgs e)
         {
 
         }
