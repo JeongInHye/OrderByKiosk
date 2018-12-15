4c522b759a3f75df97ed4682857af026619bc842
성재 형 <sungjae125@gmail.com>
2018-12-15 23:39:26 +0900

    매장 등록/삭제 만듬

--- OBK/OBK/Views/AdminView/StoreAddView.cs
+++ OBK/OBK/Views/AdminView/StoreAddView.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,6 +14,9 @@ namespace OBK.Views.AdminView
     {
         private Draw draw;
         private Form parentForm;
+        private Label lb_name, lb_city, lb_gu;
+        private TextBox tb_name, tb_city, tb_gu;
+        private Button btn_add;
         private Hashtable hashtable;
 
         public StoreAddView(Form parentForm)
@@ -25,7 +29,74 @@ namespace OBK.Views.AdminView
 
         private void getView()
         {
+            hashtable = new Hashtable();
+            hashtable.Add("text", "매장명 : ");
+            hashtable.Add("width", 110);
+            hashtable.Add("point", new Point(100, 110));
+            hashtable.Add("font", new Font("고딕", 18, FontStyle.Bold));
+            hashtable.Add("name", "lb_name");
+            lb_name = draw.getLabel1(hashtable, parentForm);
 
+            hashtable = new Hashtable();
+            hashtable.Add("width", 300);
+            hashtable.Add("point", new Point(230, 110));
+            hashtable.Add("hinttext", "매장명을 입력해주세요.");
+            hashtable.Add("font", new Font("맑은고딕", 14, FontStyle.Bold));
+            hashtable.Add("name", "tb_name");
+            tb_name = draw.getHintTextBox(hashtable, parentForm);
+
+            //==============================================================================
+
+            hashtable = new Hashtable();
+            hashtable.Add("text", "도시명 : ");
+            hashtable.Add("width", 110);
+            hashtable.Add("point", new Point(100, 190));
+            hashtable.Add("font", new Font("고딕", 18, FontStyle.Bold));
+            hashtable.Add("name", "lb_city");
+            lb_city = draw.getLabel1(hashtable, parentForm);
+
+            hashtable = new Hashtable();
+            hashtable.Add("width", 300);
+            hashtable.Add("point", new Point(230, 190));
+            hashtable.Add("hinttext", "도시명을 입력해주세요.");
+            hashtable.Add("font", new Font("맑은고딕", 14, FontStyle.Bold));
+            hashtable.Add("name", "tb_city");
+            tb_city = draw.getHintTextBox(hashtable, parentForm);
+
+            //==============================================================================
+
+            hashtable = new Hashtable();
+            hashtable.Add("text", "구 : ");
+            hashtable.Add("width", 110);
+            hashtable.Add("point", new Point(100, 270));
+            hashtable.Add("font", new Font("고딕", 18, FontStyle.Bold));
+            hashtable.Add("name", "lb_gu");
+            lb_gu = draw.getLabel1(hashtable, parentForm);
+
+            hashtable = new Hashtable();
+            hashtable.Add("width", 300);
+            hashtable.Add("point", new Point(230, 270));
+            hashtable.Add("hinttext", "구이름을 입력해주세요.");
+            hashtable.Add("font", new Font("맑은고딕", 14, FontStyle.Bold));
+            hashtable.Add("name", "tb_gu");
+            tb_gu = draw.getHintTextBox(hashtable, parentForm);
+
+            //==========================================================
+
+            hashtable = new Hashtable();
+            hashtable.Add("size", new Size(150, 50));
+            hashtable.Add("point", new Point(490, 440));
+            hashtable.Add("color", Color.FromArgb(246, 246, 246));
+            hashtable.Add("name", "btn_add");
+            hashtable.Add("text", "매장 등록");
+            hashtable.Add("font", new Font("맑은 고딕", 15, FontStyle.Regular));
+            hashtable.Add("click", (EventHandler)btn_add_Click);
+            btn_add = draw.getButton1(hashtable, parentForm);
+        }
+
+        private void btn_add_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(tb_name.Text+"지점 등록!!\n위치 : "+tb_city.Text+" "+tb_gu.Text);
         }
     }
 }
--- OBK/OBK/Views/AdminView/StoreDeleteView.cs
+++ OBK/OBK/Views/AdminView/StoreDeleteView.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,9 +14,8 @@ namespace OBK.Views.AdminView
     {
         private Draw draw;
         private Form parentForm;
-        private Label label1, label2, label3, label4, label5, label6;
-        private ListView list;
-        private Button button;
+        private ListView liststore;
+        private Button button_storedelete;
         private Hashtable hashtable;
 
         public StoreDeleteView(Form parentForm)
@@ -28,7 +28,43 @@ namespace OBK.Views.AdminView
 
         private void getView()
         {
+            hashtable = new Hashtable();
+            hashtable.Add("color", Color.WhiteSmoke);
+            hashtable.Add("size", new Size(620, 400));
+            hashtable.Add("point", new Point(30, 30));
+            hashtable.Add("name", "liststore");
+            liststore = draw.getListView(hashtable, parentForm);
+            liststore.Columns.Add("", 30, HorizontalAlignment.Center);
+            liststore.Columns.Add("매장명", 240, HorizontalAlignment.Center);
+            liststore.Columns.Add("위치", 345, HorizontalAlignment.Center);
+            liststore.Items.Add(new ListViewItem(new string[] { " ", "가산디지털점","서울특별시 금천구" }));
 
+            hashtable = new Hashtable();
+            hashtable.Add("size", new Size(150, 50));
+            hashtable.Add("point", new Point(490, 440));
+            hashtable.Add("color", Color.FromArgb(246, 246, 246));
+            hashtable.Add("name", "button_storedelete");
+            hashtable.Add("text", "매장 삭제");
+            hashtable.Add("font", new Font("맑은 고딕", 15, FontStyle.Regular));
+            hashtable.Add("click", (EventHandler)button_storedelete_click);
+            button_storedelete = draw.getButton1(hashtable, parentForm);
+        }
+        private void button_storedelete_click(object sender, EventArgs e)
+        {
+            foreach (ListViewItem listitem in liststore.Items)
+            {
+                if (liststore.Items.Count > 0)
+                {
+                    for (int i = liststore.Items.Count - 1; i >= 0; i--)
+                    {
+                        if (liststore.Items[i].Checked == true)
+                        {
+                            //MessageBox.Show(liststore.Items[i].Text);
+                            liststore.Items[i].Remove();
+                        }
+                    }
+                }
+            }
         }
     }
 }
