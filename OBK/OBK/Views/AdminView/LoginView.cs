2347db174a23bc277a6873d4904c4e723f3a2bad
정인혜 <inhyea36@naver.com>
2018-12-27 16:43:35 +0900

    Login 수정

--- OBK/OBK/Views/AdminView/LoginView.cs
+++ OBK/OBK/Views/AdminView/LoginView.cs
@@ -37,14 +37,20 @@ namespace OBK.Views.AdminView
             hashtable.Add("color", Color.LightGray);
             hashtable.Add("name", "label_title");
             hashtable.Add("text", "관리자 모드");
-            hashtable.Add("font", new Font("굴림", 15, FontStyle.Bold));
+            hashtable.Add("font", new Font("맑은 고딕", 15, FontStyle.Bold));
             label_title = draw.getLabel1(hashtable, parentForm);
             label_title.TextAlign = ContentAlignment.MiddleCenter;
 
             hashtable = new Hashtable();
-            hashtable.Add("width", 180);
+            hashtable.Add("width", 0);
+            hashtable.Add("point", new Point(0, 0));
+            hashtable.Add("name", "textBox");
+            hashtable.Add("enabled", true);
+            textBox = draw.getTextBox(hashtable, parentForm);
+
+            hashtable = new Hashtable();
+            hashtable.Add("width", 170);
             hashtable.Add("point", new Point(130,150));
-            hashtable.Add("color", Color.White);
             hashtable.Add("name", "textBox");
             hashtable.Add("enabled", true);
             textBox = draw.getTextBox(hashtable, parentForm);
@@ -56,138 +62,150 @@ namespace OBK.Views.AdminView
             hashtable.Add("color", Color.LightGray);
             hashtable.Add("name", "label_pw");
             hashtable.Add("text", "PW :");
-            hashtable.Add("font", new Font("굴림", 12, FontStyle.Bold));
+            hashtable.Add("font", new Font("맑은 고딕", 12, FontStyle.Bold));
             label_pw = draw.getLabel1(hashtable, parentForm);
             label_pw.TextAlign = ContentAlignment.MiddleCenter;
 
             hashtable = new Hashtable();
+            hashtable.Add("font", new Font("맑은 고딕", 12, FontStyle.Bold));
             hashtable.Add("size", new Size(40, 40));
             hashtable.Add("point", new Point(90, 220));
             hashtable.Add("color", Color.LightGray);
             hashtable.Add("name", "btn1");
             hashtable.Add("text", "1");
             hashtable.Add("click", (EventHandler)btn1_click);
-            btn1 = draw.getButton(hashtable, parentForm);
+            btn1 = draw.getButton1(hashtable, parentForm);
 
             hashtable = new Hashtable();
+            hashtable.Add("font", new Font("맑은 고딕", 12, FontStyle.Bold));
             hashtable.Add("size", new Size(40, 40));
             hashtable.Add("point", new Point(90, 280));
             hashtable.Add("color", Color.LightGray);
             hashtable.Add("name", "btn4");
             hashtable.Add("text", "4");
             hashtable.Add("click", (EventHandler)btn4_click);
-            btn4 = draw.getButton(hashtable, parentForm);
+            btn4 = draw.getButton1(hashtable, parentForm);
 
             hashtable = new Hashtable();
+            hashtable.Add("font", new Font("맑은 고딕", 12, FontStyle.Bold));
             hashtable.Add("size", new Size(40, 40));
             hashtable.Add("point", new Point(90, 340));
             hashtable.Add("color", Color.LightGray);
             hashtable.Add("name", "btn7");
             hashtable.Add("text", "7");
             hashtable.Add("click", (EventHandler)btn7_click);
-            btn7 = draw.getButton(hashtable, parentForm);
+            btn7 = draw.getButton1(hashtable, parentForm);
 
             hashtable = new Hashtable();
+            hashtable.Add("font", new Font("맑은 고딕", 12, FontStyle.Bold));
             hashtable.Add("size", new Size(40, 40));
             hashtable.Add("point", new Point(90, 400));
             hashtable.Add("color", Color.LightGray);
             hashtable.Add("name", "btn_clear");
             hashtable.Add("text", "C");
             hashtable.Add("click", (EventHandler)btn_clear_click);
-            btn_clear = draw.getButton(hashtable, parentForm);
+            btn_clear = draw.getButton1(hashtable, parentForm);
             //첫줄
             hashtable = new Hashtable();
+            hashtable.Add("font", new Font("맑은 고딕", 12, FontStyle.Bold));
             hashtable.Add("size", new Size(40, 40));
             hashtable.Add("point", new Point(150, 220));
             hashtable.Add("color", Color.LightGray);
             hashtable.Add("name", "btn2");
             hashtable.Add("text", "2");
             hashtable.Add("click", (EventHandler)btn2_click);
-            btn2 = draw.getButton(hashtable, parentForm);
+            btn2 = draw.getButton1(hashtable, parentForm);
 
             hashtable = new Hashtable();
+            hashtable.Add("font", new Font("맑은 고딕", 12, FontStyle.Bold));
             hashtable.Add("size", new Size(40, 40));
             hashtable.Add("point", new Point(150, 280));
             hashtable.Add("color", Color.LightGray);
             hashtable.Add("name", "btn5");
             hashtable.Add("text", "5");
             hashtable.Add("click", (EventHandler)btn5_click);
-            btn5 = draw.getButton(hashtable, parentForm);
+            btn5 = draw.getButton1(hashtable, parentForm);
            
             hashtable = new Hashtable();
+            hashtable.Add("font", new Font("맑은 고딕", 12, FontStyle.Bold));
             hashtable.Add("size", new Size(40, 40));
             hashtable.Add("point", new Point(150, 340));
             hashtable.Add("color", Color.LightGray);
             hashtable.Add("name", "btn8");
             hashtable.Add("text", "8");
             hashtable.Add("click", (EventHandler)btn8_click);
-            btn8 = draw.getButton(hashtable, parentForm);
+            btn8 = draw.getButton1(hashtable, parentForm);
 
             hashtable = new Hashtable();
+            hashtable.Add("font", new Font("맑은 고딕", 12, FontStyle.Bold));
             hashtable.Add("size", new Size(40, 40));
             hashtable.Add("point", new Point(150, 400));
             hashtable.Add("color", Color.LightGray);
             hashtable.Add("name", "btn0");
             hashtable.Add("text", "0");
             hashtable.Add("click", (EventHandler)btn0_click);
-            btn0 = draw.getButton(hashtable, parentForm);
+            btn0 = draw.getButton1(hashtable, parentForm);
 
             hashtable = new Hashtable();
+            hashtable.Add("font", new Font("맑은 고딕", 12, FontStyle.Bold));
             hashtable.Add("size", new Size(40, 40));
             hashtable.Add("point", new Point(210, 220));
             hashtable.Add("color", Color.LightGray);
             hashtable.Add("name", "btn3");
             hashtable.Add("text", "3");
             hashtable.Add("click", (EventHandler)btn3_click);
-            btn3 = draw.getButton(hashtable, parentForm);
+            btn3 = draw.getButton1(hashtable, parentForm);
 
         
             hashtable = new Hashtable();
+            hashtable.Add("font", new Font("맑은 고딕", 12, FontStyle.Bold));
             hashtable.Add("size", new Size(40, 40));
             hashtable.Add("point", new Point(210, 280));
             hashtable.Add("color", Color.LightGray);
             hashtable.Add("name", "btn6");
             hashtable.Add("text", "6");
             hashtable.Add("click", (EventHandler)btn6_click);
-            btn6 = draw.getButton(hashtable, parentForm);
+            btn6 = draw.getButton1(hashtable, parentForm);
 
             hashtable = new Hashtable();
+            hashtable.Add("font", new Font("맑은 고딕", 12, FontStyle.Bold));
             hashtable.Add("size", new Size(40, 40));
             hashtable.Add("point", new Point(210, 340));
             hashtable.Add("color", Color.LightGray);
             hashtable.Add("name", "btn9");
             hashtable.Add("text", "9");
             hashtable.Add("click", (EventHandler)btn9_click);
-            btn9 = draw.getButton(hashtable, parentForm);
+            btn9 = draw.getButton1(hashtable, parentForm);
 
 
             hashtable = new Hashtable();
+            hashtable.Add("font", new Font("맑은 고딕", 12, FontStyle.Bold));
             hashtable.Add("size", new Size(40, 40));
             hashtable.Add("point", new Point(210, 400));
             hashtable.Add("color", Color.LightGray);
             hashtable.Add("name", "btn_erase");
             hashtable.Add("text", "←");
             hashtable.Add("click", (EventHandler)btn_erase_click);
-            btn_erase = draw.getButton(hashtable, parentForm);
+            btn_erase = draw.getButton1(hashtable, parentForm);
             //세번째줄
-            //============숫자버튼 생성 완료====================
             
+            //============숫자버튼 생성 완료====================
             hashtable = new Hashtable();
+            hashtable.Add("font", new Font("맑은 고딕", 12, FontStyle.Bold));
             hashtable.Add("size", new Size(40, 220));
             hashtable.Add("point", new Point(270, 220));
             hashtable.Add("color", Color.LightGray);
             hashtable.Add("name", "btn_login");
-            hashtable.Add("text", "L\n\no\n\ng\n\ni\n\nn");
+            hashtable.Add("text", "로\n그\n인");
             hashtable.Add("click", (EventHandler)btn_login_click);
-            btn_login = draw.getButton(hashtable, parentForm);
+            btn_login = draw.getButton1(hashtable, parentForm);
 
         }
 
         private void btn_login_click(object sender, EventArgs e)
         {
             WebAPI api = new WebAPI();
-            string Correctpw = api.getPasswd("http://192.168.3.31:5000/admin/passwd");
-
+            string Correctpw = api.getPasswd(Program.serverUrl+"admin/passwd");
 
             if (passwd == Correctpw)
             {
