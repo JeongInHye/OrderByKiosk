e06cf21aa54e78c7017578f4af6566f595752db3
정인혜 <inhyea36@naver.com>
2018-12-28 11:21:46 +0900

    Menu출력되는 글씨체 바꿈

--- OBK/OBK/Views/AdminView/MenuEditView.cs
+++ OBK/OBK/Views/AdminView/MenuEditView.cs
@@ -152,9 +152,9 @@ namespace OBK.Views.AdminView
             hashtable = new Hashtable();
             hashtable.Add("size", new Size(50, 50));
             hashtable.Add("point", new Point(550, 260));
-            hashtable.Add("color", Color.FromArgb(246, 246, 246));
+            hashtable.Add("color", Color.White);
             hashtable.Add("name", "btnAdd");
-            hashtable.Add("text", "이미지첨부");
+            hashtable.Add("text", "");
             hashtable.Add("font", new Font("맑은 고딕", 10, FontStyle.Regular));
             hashtable.Add("click", (EventHandler)btnImgAdd_Click);
             btnImgAdd = draw.getButton1(hashtable, panelTwo);
